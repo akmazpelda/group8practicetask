753eb86c0061e94e0d68260325d2e1d25ec0bbc7
veraPelda <akmazpelda@gmail.com>
2024-05-29 23:44:15 -0500

    week1 tasks

--- src/week1/finra.java
+++ src/week1/finra.java
@@ -2,12 +2,12 @@ package week1;
 
 public class finra {
 
-    /* public static void FINRA() {
-    string result = "" ;
-    for (int i=1; i<= 30; i++) {
-    result += (i % 5 ==0 && i %3 ==0) ? "RA " : (i%3 == 0) ? "FIN" : i+" ";
-    }
+    /* Write a function which prints out the numbers from 1 to 30 but
+    for numbers which are a multiple of 3, print "FIN" instead of the
+    number and for numbers which are a multiple of 5, print "RA" instead of
+    the numbers which are a multiple of both 3 and 5, print "FINRA" instead of
+    the number.
 
-    systeam.outo.print(result);
+
        */
 }
